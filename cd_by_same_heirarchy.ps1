c751345a83dc05062c3f0b38b4be7c18c14dd2ca
ak47xm4 <ak47xm4@gmail.com>
2025-11-22 12:42:42 +0800

    first commit

---
+++ cd_by_same_heirarchy.ps1
@@ -0,0 +1,17 @@
+# 1 - 1 Get the current directory
+
+# 2 - 1 select how many levels up to go
+
+# 2 - 2 save the folder hierarchy structure as a variable: $folder_path_4_same_hierarchy
+
+# 3 - 1 get the directories name
+
+# 3 - 2 list the directories for selection
+
+# 3 - 3 select the directory to go to
+
+# 4 - 1 go to the directory
+
+# 4 - 2 error handling: if the directory does not exist, show a message and exit
+
+# 5 - 1 command: oc "final_path"
